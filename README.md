b3d4585fcf09299463e48ff11583e99b27e4c412
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-11-12 12:07:54 +0100

    Update and cleanup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Sample project showing a nullness error visible in IntelliJ or when running `./gradlew build`.
+Sample Gradle project showing NullAway configured with the JSpecify mode via the [Spring nullability plugin](https://github.com/spring-gradle-plugins/nullability-plugin).
+A nullness issue is visible in IDEs with support for JSpecify or when running `./gradlew build`.
 
 For Maven, see a sample `pom.xml` [here](https://github.com/sdeleuze/jspecify-nullway-demo/blob/maven/pom.xml). 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id 'java'
-	id 'io.spring.nullability' version '0.0.8'
+    id("java")
+	id("io.spring.nullability") version "0.0.8"
 }
 
-group = 'org.example'
-version = '1.0-SNAPSHOT'
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -13,16 +13,16 @@ repositories {
 java {
 	toolchain {
 		// Usage of a recent javac version for compatibility with NullAway JSpecify mode
-		languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(25)
 	}
 }
 
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
 	// Keep a JDK 17 baseline
 	options.release = 17
 }
 
 dependencies {
 	// https://jspecify.dev/
-    implementation 'org.jspecify:jspecify:1.0.0'
+    implementation("org.jspecify:jspecify:1.0.0")
 }
