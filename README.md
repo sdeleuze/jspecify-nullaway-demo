627af2e52934e6361f2a6d9f29d8a7ba14fb8cee
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-11-12 12:18:33 +0100

    Update and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Sample project showing a nullness error visible in IntelliJ or when running `./mvnw clean package`.
+Sample Maven project showing NullAway configured with the JSpecify mode.
+A nullness issue is visible in IDEs with support for JSpecify or when running `./mvnw clean package`.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		TokenExtractor extractor = new DefaultTokenExtractor();
 		String token = extractor.extractToken("...");
+		// Uncomment the if statement to fix the nullability issue
+		// if (token != null) {
 		System.out.println("The token has a length of " + token.length());
+		// }
 	}
 }
