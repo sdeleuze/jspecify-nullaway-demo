fb0846087b08ce3865c0f604d190a8621c472fe5
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2026-04-11 17:34:46 +0200

    Fix indents

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,37 @@
 import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
-    id("java")
-    id("net.ltgt.errorprone") version "5.1.0" // https://github.com/tbroyer/gradle-errorprone-plugin
+	id("java")
+	id("net.ltgt.errorprone") version "5.1.0" // https://github.com/tbroyer/gradle-errorprone-plugin
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 java {
 	toolchain {
 		// Usage of a recent javac version for compatibility with NullAway JSpecify mode
-        languageVersion = JavaLanguageVersion.of(25)
+		languageVersion = JavaLanguageVersion.of(25)
 	}
 }
 
 tasks.withType<JavaCompile> {
-    options.errorprone  {
-        disableAllChecks = true // Other error prone checks are disabled
-        option("NullAway:OnlyNullMarked", "true") // Enable nullness checks only in null-marked code
-        error("NullAway") // bump checks from warnings (default) to errors
-        option("NullAway:JSpecifyMode", "true") // https://github.com/uber/NullAway/wiki/JSpecify-Support
-    }
+	options.errorprone  {
+		disableAllChecks = true // Other error prone checks are disabled
+		option("NullAway:OnlyNullMarked", "true") // Enable nullness checks only in null-marked code
+		error("NullAway") // bump checks from warnings (default) to errors
+		option("NullAway:JSpecifyMode", "true") // https://github.com/uber/NullAway/wiki/JSpecify-Support
+	}
 	// Keep a JDK 17 baseline
 	options.release = 17
 }
 
 dependencies {
-    implementation("org.jspecify:jspecify:1.0.0") // https://jspecify.dev/
-    errorprone("com.google.errorprone:error_prone_core:2.42.0") // https://github.com/google/error-prone
-    errorprone("com.uber.nullaway:nullaway:0.13.2") // https://github.com/uber/NullAway
+	implementation("org.jspecify:jspecify:1.0.0") // https://jspecify.dev/
+	errorprone("com.google.errorprone:error_prone_core:2.42.0") // https://github.com/google/error-prone
+	errorprone("com.uber.nullaway:nullaway:0.13.2") // https://github.com/uber/NullAway
 }
